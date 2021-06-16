e050f685e169a8e888c7e3391c578df416189f6a
MarkelWhit <litslayer@gmail.com>
2021-06-16 18:50:07 -0500

    working on post input

--- advancedform/src/App.js
+++ advancedform/src/App.js
@@ -1,7 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form'
+import React, { useEffect, useState } from 'react';
+import * as yup from 'yup';
+import schema from './formSchema';
+
+const initialFormValues = {
+  ///text inputs///
+  username: '',
+  email: '',
+  password: '',
+  ///checkboxes///
+  terms: false ,
+}
+
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+
+const initialUsers = []
+
+const initialDisabled = true
 
 function App() {
+
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const validate = (name, value) =>{
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
+  }
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  },[formValues])
+
+  const onChange = (name, value) => {
+    validate(name, value) 
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const onSubmit = () => {
+    const newUser = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: formValues.terms
+    }
+    console.log(newUser)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +77,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <Form 
+      values={formValues}
+      change={onChange}
+      submit={onSubmit}
+      disabled={disabled}
+      errors={formErrors}/>
     </div>
   );
 }
---
+++ advancedform/src/Form.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+export default function Form(props) {
+   const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+    } = props
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        change(name, valueToUse)
+    }
+
+
+    return (
+        <form className='User form' onSubmit={onSubmit}>
+            <div className='form submit'>
+                <h2>User Onboarding</h2>
+            <div className='errors'>
+                <div>{errors.username}</div>
+                <div>{errors.email}</div>
+                <div>{errors.password}</div>
+                <div>{errors.terms}</div>
+            </div>
+            <label>Username
+                <input
+                value={values.username}
+                onchange={onChange}
+                name='username'
+                type='text'
+                />
+            </label>
+            <label>Email
+                <input
+                value={values.email}
+                onChange={onChange}
+                name='email'
+                type='email'
+                />
+            </label>
+            <label>Password
+                <input
+                value={values.password}
+                onchange={onChange}
+                name='password'
+                type='password'
+                />
+            </label>
+            <label> 'Terms of Service'
+                <input
+                type='checkbox'
+                name='terms'
+                checked={values.terms}
+                onChange={onChange}
+                />
+            </label>
+
+                <button disabled={disabled}>Create User</button>
+            </div>
+            <br/>
+        </form>
+    
+    )
+}
